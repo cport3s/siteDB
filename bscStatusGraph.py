bcecd50145a2670fc4bb8a726ed48e045f2e3cd1
Carlos Portes <carlosportes.c@gmail.com>
2020-11-02 20:59:09 -0400

    2020110202
    -Completed: Auto refresh on graph, pulling data from db
    -In Progress: Time Frame Dropdown selector

--- bscStatusGraph.py
+++ bscStatusGraph.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -19,33 +18,35 @@ dbpassword = 'BSCAltice.123'
 hostip = '172.16.121.41'
 dbname = 'ran_pf_data'
 
-# Connect to DB
-connectr = mysql.connector.connect(user = dbusername, password = dbpassword, host = hostip, database = dbname)
-# Connection must be buffered when executing multiple querys on DB before closing connection.
-pointer = connectr.cursor(buffered=True)
-
 app.layout = html.Div(children=[
     html.H1(children='BSC KPIs'),
     html.Div(children='''Number of LU requests.'''),
+    dcc.Dropdown(
+        options=[{'label':'1 Day', 'value':'1'}, {'label':'3 Days', 'value':'3'}, {'label':'7 Days', 'value':'7'}, {'label':'30 Days', 'value':'30'}], 
+        value='timeFrameDropdown'
+    ),
     #dcc.Graph(id='example-graph', figure=fig)
-    dcc.Graph(id='live-graph'),
-    dcc.Interval(id='graph-update', interval=30000, n_intervals=0)
+    dcc.Graph(id='liveGraph'),
+    dcc.Interval(id='upateInterval', interval=300000, n_intervals=0)
 ])
 
-@app.callback(Output('live-graph', 'figure'), [Input('graph-update', 'n_intervals')])
+@app.callback(Output('liveGraph', 'figure'), [Input('upateInterval', 'n_intervals')])
 def updateGraphData(n):
+    # Connect to DB
+    connectr = mysql.connector.connect(user = dbusername, password = dbpassword, host = hostip, database = dbname)
+    # Connection must be buffered when executing multiple querys on DB before closing connection.
+    pointer = connectr.cursor(buffered=True)
     pointer.execute('SELECT luupdaterequests, lastupdate FROM ran_pf_data.bsc_performance_data where nename = \'BSC_01_RRA\';')
     queryRaw = pointer.fetchall()
     queryPayload = np.array(queryRaw)
     df = pd.DataFrame({ 'LU Requests':queryPayload[:,0], 'Time':queryPayload[:,1] })
     fig = px.bar(df, x="Time", y="LU Requests")
+    fig.update_layout()
     queryRaw.clear()
+    # Close DB connection
+    pointer.close()
+    connectr.close()
     return fig
-
-#queryRaw.clear()
-## Close DB connection
-#pointer.close()
-#connectr.close()
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port='5005')
