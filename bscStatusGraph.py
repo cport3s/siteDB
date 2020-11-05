efa24121f998be54f047895ffb7dd86ffc162996
Carlos Portes <carlosportes.c@gmail.com>
2020-11-05 16:44:15 -0400

    2020110502
    -Completed: RNC Graph Implementation & Graph Scrolling.
    -Up next: Substitute flexbox for grid.

--- bscStatusGraph.py
+++ bscStatusGraph.py
@@ -204,14 +204,132 @@ def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
     connectr.close()
     return fig1, fig2, fig3, fig4, fig5, fig6
 
+# We pass value from the time frame dropdown because it gets updated everytime you change the seleccion on the drop down.
+@app.callback([Output('rnc01Graph', 'figure'), Output('rnc02Graph', 'figure'), Output('rnc03Graph', 'figure'), Output('rnc04Graph', 'figure'), Output('rnc05Graph', 'figure'), Output('rnc06Graph', 'figure'), Output('rnc07Graph', 'figure')], [Input('upateInterval', 'n_intervals'), Input('timeFrameDropdown', 'value'), Input('dataTypeDropdown', 'value')])
+def updateGraphData_rnc(currentInterval, timeFrameDropdown, dataTypeDropdown):
+    conversionDict = {'cssr':'csconnectionsuccessrate', 'dcr':'csdropcallrate', 'assignmentsuccessrate':'rrcconnectionsuccessrate', 'luupdatesr':'pagingsuccessrate'}
+    daysDelta = int(timeFrameDropdown)
+    # starttime is the current date/time - daysdelta
+    startTime = (datetime.now() - timedelta(days=daysDelta)).strftime("%Y/%m/%d %H:%M:%S")
+    # Connect to DB
+    connectr = mysql.connector.connect(user = dbusername, password = dbpassword, host = hostip, database = dbname)
+    # Connection must be buffered when executing multiple querys on DB before closing connection.
+    pointer = connectr.cursor(buffered=True)
+    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_01_RRA\' and lastupdate >= \'' + startTime + '\';')
+    queryRaw = pointer.fetchall()
+    queryPayload = np.array(queryRaw)
+    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
+    fig1 = px.bar(df, x="Time", y='Value', title='RNC_01_RRA')
+    # Set Graph background colores & title font size
+    fig1.update_layout(
+        plot_bgcolor='#000000', 
+        paper_bgcolor='#000000', 
+        font_color='#FFFFFF', 
+        title_font_size=graphTitleFontSize
+    )
+    # Color the graph
+    fig1.update_traces(marker_color='#17FF00')
+    queryRaw.clear()
+    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_02_STGO\' and lastupdate >= \'' + startTime + '\';')
+    queryRaw = pointer.fetchall()
+    queryPayload = np.array(queryRaw)
+    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
+    fig2 = px.bar(df, x="Time", y='Value', title='RNC_02_STGO')
+    # Set Graph background colores & title font size
+    fig2.update_layout(
+        plot_bgcolor='#000000', 
+        paper_bgcolor='#000000', 
+        font_color='#FFFFFF', 
+        title_font_size=graphTitleFontSize
+    )
+    # Color the graph
+    fig2.update_traces(marker_color='#17FF00')
+    queryRaw.clear()
+    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_03_VM\' and lastupdate >= \'' + startTime + '\';')
+    queryRaw = pointer.fetchall()
+    queryPayload = np.array(queryRaw)
+    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
+    fig3 = px.bar(df, x="Time", y='Value', title='RNC_03_VM')
+    # Set Graph background colores & title font size
+    fig3.update_layout(
+        plot_bgcolor='#000000', 
+        paper_bgcolor='#000000', 
+        font_color='#FFFFFF', 
+        title_font_size=graphTitleFontSize
+    )
+    # Color the graph
+    fig3.update_traces(marker_color='#17FF00')
+    queryRaw.clear()
+    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_04_VM\' and lastupdate >= \'' + startTime + '\';')
+    queryRaw = pointer.fetchall()
+    queryPayload = np.array(queryRaw)
+    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
+    fig4 = px.bar(df, x="Time", y='Value', title='RNC_04_VM')
+    # Set Graph background colores & title font size
+    fig4.update_layout(
+        plot_bgcolor='#000000', 
+        paper_bgcolor='#000000', 
+        font_color='#FFFFFF', 
+        title_font_size=graphTitleFontSize
+    )
+    # Color the graph
+    fig4.update_traces(marker_color='#17FF00')
+    queryRaw.clear()
+    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_05_RRA\' and lastupdate >= \'' + startTime + '\';')
+    queryRaw = pointer.fetchall()
+    queryPayload = np.array(queryRaw)
+    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
+    fig5 = px.bar(df, x="Time", y='Value', title='RNC_05_RRA')
+    # Set Graph background colores & title font size
+    fig5.update_layout(
+        plot_bgcolor='#000000', 
+        paper_bgcolor='#000000', 
+        font_color='#FFFFFF', 
+        title_font_size=graphTitleFontSize
+    )
+    # Color the graph
+    fig5.update_traces(marker_color='#17FF00')
+    queryRaw.clear()
+    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_06_STGO\' and lastupdate >= \'' + startTime + '\';')
+    queryRaw = pointer.fetchall()
+    queryPayload = np.array(queryRaw)
+    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
+    fig6 = px.bar(df, x="Time", y='Value', title='RNC_06_STGO')
+    # Set Graph background colores & title font size
+    fig6.update_layout(
+        plot_bgcolor='#000000', 
+        paper_bgcolor='#000000', 
+        font_color='#FFFFFF', 
+        title_font_size=graphTitleFontSize
+    )
+    # Color the graph
+    fig6.update_traces(marker_color='#17FF00')
+    queryRaw.clear()
+    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_07_VM\' and lastupdate >= \'' + startTime + '\';')
+    queryRaw = pointer.fetchall()
+    queryPayload = np.array(queryRaw)
+    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
+    fig7 = px.bar(df, x="Time", y='Value', title='RNC_07_VM')
+    # Set Graph background colores & title font size
+    fig7.update_layout(
+        plot_bgcolor='#000000', 
+        paper_bgcolor='#000000', 
+        font_color='#FFFFFF', 
+        title_font_size=graphTitleFontSize
+    )
+    # Color the graph
+    fig7.update_traces(marker_color='#17FF00')
+    # Close DB connection
+    pointer.close()
+    connectr.close()
+    return fig1, fig2, fig3, fig4, fig5, fig6, fig7
 
-
-@app.callback([Output('bsc01Graph', 'style'), Output('bsc02Graph', 'style'), Output('bsc03Graph', 'style'), Output('bsc04Graph', 'style'), Output('bsc05Graph', 'style'), Output('bsc06Graph', 'style')], [Input('upateInterval', 'n_intervals')])
+@app.callback([Output('bsc01Graph', 'style'), Output('bsc02Graph', 'style'), Output('bsc03Graph', 'style'), Output('bsc04Graph', 'style'), Output('bsc05Graph', 'style'), Output('bsc06Graph', 'style'), Output('rnc01Graph', 'style'), Output('rnc02Graph', 'style'), Output('rnc03Graph', 'style'), Output('rnc04Graph', 'style'), Output('rnc05Graph', 'style'), Output('rnc06Graph', 'style'), Output('rnc07Graph', 'style')], [Input('upateInterval', 'n_intervals')])
 def hideGraph(currentInterval):
     if currentInterval%2 == 0:
-        return {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}
+        return {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}
     else:
-        return {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}
+        return {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port='5005')
