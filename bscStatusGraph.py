f3cb9054f6ddf02b5e91a22b9a609960841f9dfd
Carlos Portes <carlosportes.c@gmail.com>
2020-11-10 12:00:46 -0400

    2020111001
    -Fixed dropdown styles.

--- bscStatusGraph.py
+++ bscStatusGraph.py
@@ -34,7 +34,7 @@ app.layout = html.Div(children=[
                 options=[{'label':'1 Day', 'value':'1'}, {'label':'3 Days', 'value':'3'}, {'label':'7 Days', 'value':'7'}, {'label':'30 Days', 'value':'30'}],
                 # value var is the default value for the drop down.
                 value='3',
-                style={'width': '50%'}
+                style={'width': '100%', 'font-size': '54px'}
             ),
             dcc.Dropdown(
                 id='dataTypeDropdown',
@@ -45,7 +45,7 @@ app.layout = html.Div(children=[
                     {'label':'Location Update Success Rate', 'value':'Location Update Success Rate'}
                     ],
                 value='Call Setup Success Rate',
-                style={'width': '50%'}
+                style={'width': '100%', 'font-size': '54px'}
             )
         ]
     ),
@@ -94,8 +94,13 @@ app.layout = html.Div(children=[
         ]
     ),
     dcc.Interval(
-        id='upateInterval', 
+        id='dataUpateInterval', 
         interval=300000, 
+        n_intervals=0
+    ),
+    dcc.Interval(
+        id='graphUpateInterval', 
+        interval=60000, 
         n_intervals=0
     )
 ])
@@ -107,20 +112,28 @@ app.layout = html.Div(children=[
         Output('bsc03Graph', 'figure'), 
         Output('bsc04Graph', 'figure'), 
         Output('bsc05Graph', 'figure'), 
-        Output('bsc06Graph', 'figure')
+        Output('bsc06Graph', 'figure'),
+        Output('rnc01Graph', 'figure'), 
+        Output('rnc02Graph', 'figure'), 
+        Output('rnc03Graph', 'figure'), 
+        Output('rnc04Graph', 'figure'), 
+        Output('rnc05Graph', 'figure'), 
+        Output('rnc06Graph', 'figure'),
+        Output('rnc07Graph', 'figure')
     ], 
     [
         # We use the update interval function and both dropdown menus as inputs for the callback
-        Input('upateInterval', 'n_intervals'), 
+        Input('dataUpateInterval', 'n_intervals'), 
         Input('timeFrameDropdown', 'value'), 
-        Input('dataTypeDropdown', 'value')]
-    )
+        Input('dataTypeDropdown', 'value')
+    ])
 def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
     bscNameList = ['BSC_01_RRA', 'BSC_02_STGO', 'BSC_03_VM', 'BSC_04_VM', 'BSC_05_RRA', 'BSC_06_STGO']
     rncNameList = ['RNC_01_RRA', 'RNC_02_STGO', 'RNC_03_VM', 'RNC_04_VM', 'RNC_05_RRA', 'RNC_06_STGO', 'RNC_07_VM']
     gsmGraphValueConversionDict = {'Call Setup Success Rate':'cssr', 'Drop Call Rate':'dcr', 'Assignment Rate':'assignmentsuccessrate', 'Location Update Success Rate':'luupdatesr'}
-    umtsGraphValueconversionDict = {'Call Setup Success Rate':'csconnectionsuccessrate', 'Drop Call Rate':'csdropcallrate', 'Assignment Rate':'rrcconnectionsuccessrate', 'Location Update Success Rate':'pagingsuccessrate'}
+    umtsGraphValueConversionDict = {'Call Setup Success Rate':'csconnectionsuccessrate', 'Drop Call Rate':'csdropcallrate', 'Assignment Rate':'rrcconnectionsuccessrate', 'Location Update Success Rate':'pagingsuccessrate'}
     bscGraphList = []
+    rncGraphList = []
     daysDelta = int(timeFrameDropdown)
     # starttime is the current date/time - daysdelta
     startTime = (datetime.now() - timedelta(days=daysDelta)).strftime("%Y/%m/%d %H:%M:%S")
@@ -128,12 +141,13 @@ def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
     connectr = mysql.connector.connect(user = dbusername, password = dbpassword, host = hostip, database = dbname)
     # Connection must be buffered when executing multiple querys on DB before closing connection.
     pointer = connectr.cursor(buffered=True)
-    for ne in bscNameList:
-        pointer.execute('SELECT ' + gsmGraphValueConversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.bsc_performance_data where nename = \'' + ne + '\' and lastupdate >= \'' + startTime + '\';')
+    for bsc in bscNameList:
+        pointer.execute('SELECT ' + gsmGraphValueConversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.bsc_performance_data where nename = \'' + bsc + '\' and lastupdate >= \'' + startTime + '\';')
         queryRaw = pointer.fetchall()
         queryPayload = np.array(queryRaw)
+        # Transform the query payload into a dataframe
         df = pd.DataFrame({ dataTypeDropdown:queryPayload[:,0], 'Time':queryPayload[:,1] })
-        fig = px.bar(df, x="Time", y=dataTypeDropdown, title=ne)
+        fig = px.bar(df, x="Time", y=dataTypeDropdown, title=bsc)
         # Set Graph background colores & title font size
         fig.update_layout(
             plot_bgcolor='#000000', 
@@ -146,132 +160,48 @@ def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
         # Append the current graph to the graph list
         bscGraphList.append(fig)
         queryRaw.clear()
+    for rnc in rncNameList:
+        pointer.execute('SELECT ' + umtsGraphValueConversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'' + rnc + '\' and lastupdate >= \'' + startTime + '\';')
+        queryRaw = pointer.fetchall()
+        queryPayload = np.array(queryRaw)
+        # Transform the query payload into a dataframe
+        df = pd.DataFrame({ dataTypeDropdown:queryPayload[:,0], 'Time':queryPayload[:,1] })
+        fig = px.bar(df, x="Time", y=dataTypeDropdown, title=rnc)
+        # Set Graph background colores & title font size
+        fig.update_layout(
+            plot_bgcolor='#000000', 
+            paper_bgcolor='#000000', 
+            font_color='#FFFFFF', 
+            title_font_size=54
+        )
+        # Color the graph
+        fig.update_traces(marker_color='#17FF00')
+        # Append the current graph to the graph list
+        rncGraphList.append(fig)
+        queryRaw.clear()
     # Close DB connection
     pointer.close()
     connectr.close()
-    return bscGraphList[0], bscGraphList[1], bscGraphList[2], bscGraphList[3], bscGraphList[4], bscGraphList[5]
+    return bscGraphList[0], bscGraphList[1], bscGraphList[2], bscGraphList[3], bscGraphList[4], bscGraphList[5], rncGraphList[0], rncGraphList[1], rncGraphList[2], rncGraphList[3], rncGraphList[4], rncGraphList[5], rncGraphList[6]
 
-# We pass value from the time frame dropdown because it gets updated everytime you change the seleccion on the drop down.
-@app.callback([Output('rnc01Graph', 'figure'), Output('rnc02Graph', 'figure'), Output('rnc03Graph', 'figure'), Output('rnc04Graph', 'figure'), Output('rnc05Graph', 'figure'), Output('rnc06Graph', 'figure'), Output('rnc07Graph', 'figure')], [Input('upateInterval', 'n_intervals'), Input('timeFrameDropdown', 'value'), Input('dataTypeDropdown', 'value')])
-def updateGraphData_rnc(currentInterval, timeFrameDropdown, dataTypeDropdown):
-    conversionDict = {'Call Setup Success Rate':'csconnectionsuccessrate', 'Drop Call Rate':'csdropcallrate', 'Assignment Rate':'rrcconnectionsuccessrate', 'Location Update Success Rate':'pagingsuccessrate'}
-    daysDelta = int(timeFrameDropdown)
-    # starttime is the current date/time - daysdelta
-    startTime = (datetime.now() - timedelta(days=daysDelta)).strftime("%Y/%m/%d %H:%M:%S")
-    # Connect to DB
-    connectr = mysql.connector.connect(user = dbusername, password = dbpassword, host = hostip, database = dbname)
-    # Connection must be buffered when executing multiple querys on DB before closing connection.
-    pointer = connectr.cursor(buffered=True)
-    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_01_RRA\' and lastupdate >= \'' + startTime + '\';')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
-    fig1 = px.bar(df, x="Time", y='Value', title='RNC_01_RRA')
-    # Set Graph background colores & title font size
-    fig1.update_layout(
-        plot_bgcolor='#000000', 
-        paper_bgcolor='#000000', 
-        font_color='#FFFFFF', 
-        title_font_size=graphTitleFontSize
-    )
-    # Color the graph
-    fig1.update_traces(marker_color='#17FF00')
-    queryRaw.clear()
-    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_02_STGO\' and lastupdate >= \'' + startTime + '\';')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
-    fig2 = px.bar(df, x="Time", y='Value', title='RNC_02_STGO')
-    # Set Graph background colores & title font size
-    fig2.update_layout(
-        plot_bgcolor='#000000', 
-        paper_bgcolor='#000000', 
-        font_color='#FFFFFF', 
-        title_font_size=graphTitleFontSize
-    )
-    # Color the graph
-    fig2.update_traces(marker_color='#17FF00')
-    queryRaw.clear()
-    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_03_VM\' and lastupdate >= \'' + startTime + '\';')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
-    fig3 = px.bar(df, x="Time", y='Value', title='RNC_03_VM')
-    # Set Graph background colores & title font size
-    fig3.update_layout(
-        plot_bgcolor='#000000', 
-        paper_bgcolor='#000000', 
-        font_color='#FFFFFF', 
-        title_font_size=graphTitleFontSize
-    )
-    # Color the graph
-    fig3.update_traces(marker_color='#17FF00')
-    queryRaw.clear()
-    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_04_VM\' and lastupdate >= \'' + startTime + '\';')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
-    fig4 = px.bar(df, x="Time", y='Value', title='RNC_04_VM')
-    # Set Graph background colores & title font size
-    fig4.update_layout(
-        plot_bgcolor='#000000', 
-        paper_bgcolor='#000000', 
-        font_color='#FFFFFF', 
-        title_font_size=graphTitleFontSize
-    )
-    # Color the graph
-    fig4.update_traces(marker_color='#17FF00')
-    queryRaw.clear()
-    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_05_RRA\' and lastupdate >= \'' + startTime + '\';')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
-    fig5 = px.bar(df, x="Time", y='Value', title='RNC_05_RRA')
-    # Set Graph background colores & title font size
-    fig5.update_layout(
-        plot_bgcolor='#000000', 
-        paper_bgcolor='#000000', 
-        font_color='#FFFFFF', 
-        title_font_size=graphTitleFontSize
-    )
-    # Color the graph
-    fig5.update_traces(marker_color='#17FF00')
-    queryRaw.clear()
-    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_06_STGO\' and lastupdate >= \'' + startTime + '\';')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
-    fig6 = px.bar(df, x="Time", y='Value', title='RNC_06_STGO')
-    # Set Graph background colores & title font size
-    fig6.update_layout(
-        plot_bgcolor='#000000', 
-        paper_bgcolor='#000000', 
-        font_color='#FFFFFF', 
-        title_font_size=graphTitleFontSize
-    )
-    # Color the graph
-    fig6.update_traces(marker_color='#17FF00')
-    queryRaw.clear()
-    pointer.execute('SELECT ' + conversionDict[dataTypeDropdown] + ', lastupdate FROM ran_pf_data.rnc_performance_data where nename = \'RNC_07_VM\' and lastupdate >= \'' + startTime + '\';')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    df = pd.DataFrame({ 'Value':queryPayload[:,0], 'Time':queryPayload[:,1] })
-    fig7 = px.bar(df, x="Time", y='Value', title='RNC_07_VM')
-    # Set Graph background colores & title font size
-    fig7.update_layout(
-        plot_bgcolor='#000000', 
-        paper_bgcolor='#000000', 
-        font_color='#FFFFFF', 
-        title_font_size=graphTitleFontSize
-    )
-    # Color the graph
-    fig7.update_traces(marker_color='#17FF00')
-    # Close DB connection
-    pointer.close()
-    connectr.close()
-    return fig1, fig2, fig3, fig4, fig5, fig6, fig7
-
-@app.callback([Output('bsc01Graph', 'style'), Output('bsc02Graph', 'style'), Output('bsc03Graph', 'style'), Output('bsc04Graph', 'style'), Output('bsc05Graph', 'style'), Output('bsc06Graph', 'style'), Output('rnc01Graph', 'style'), Output('rnc02Graph', 'style'), Output('rnc03Graph', 'style'), Output('rnc04Graph', 'style'), Output('rnc05Graph', 'style'), Output('rnc06Graph', 'style'), Output('rnc07Graph', 'style')], [Input('upateInterval', 'n_intervals')])
+@app.callback([
+        Output('bsc01Graph', 'style'), 
+        Output('bsc02Graph', 'style'), 
+        Output('bsc03Graph', 'style'), 
+        Output('bsc04Graph', 'style'), 
+        Output('bsc05Graph', 'style'), 
+        Output('bsc06Graph', 'style'), 
+        Output('rnc01Graph', 'style'), 
+        Output('rnc02Graph', 'style'), 
+        Output('rnc03Graph', 'style'), 
+        Output('rnc04Graph', 'style'), 
+        Output('rnc05Graph', 'style'), 
+        Output('rnc06Graph', 'style'), 
+        Output('rnc07Graph', 'style')
+    ], 
+    [
+        Input('graphUpateInterval', 'n_intervals')
+    ])
 def hideGraph(currentInterval):
     if currentInterval%2 == 0:
         return {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'inline'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}, {'display':'none'}
