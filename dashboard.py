468d3da06e280e0c2f619fcc629f423f49a57f23
Carlos Portes <carlosportes.c@gmail.com>
2020-12-18 14:49:42 -0400

    2020121801
    -Backup

--- dashboard.py
+++ dashboard.py
@@ -30,8 +30,11 @@ rncNameList = ['RNC_01_RRA', 'RNC_02_STGO', 'RNC_03_VM', 'RNC_04_VM', 'RNC_05_RR
 ranReportFilepath = "D:\\ftproot\\BSC\\ran_report\\"
 latestRanReport = ranReportFilepath + os.listdir(ranReportFilepath)[0]
 ranReportLteTable = pd.read_excel(latestRanReport, sheet_name='4G Table')
+ranReportLteTable['Threshold'] = ['< 0.13%', '>= 99.00%', '>= 99%', '', '>= 6500.0000', '', '', '', '', '', '']
 ranReportUmtsTable = pd.read_excel(latestRanReport, sheet_name='3G Table')
+ranReportUmtsTable['Threshold'] = ['< 0.17%', '>= 99.87%', '', '', '', '', '<= 0.30%', '<= 0.30%', '>= 99%', '>= 99%', '', '', '']
 ranReportGsmTable = pd.read_excel(latestRanReport, sheet_name='2G Table')
+ranReportGsmTable['Threshold'] = ['>= 99.87%', '>= 99.87%', '', '', '', '']
 #ranReportLteReport = pd.read_excel(latestRanReport, sheet_name='4G Report')
 ranReportLteColumns = [{'name': i, 'id': i} for i in ranReportLteTable.columns]
 ranReportUmtsColumns = [{'name': i, 'id': i} for i in ranReportUmtsTable.columns]
@@ -40,67 +43,6 @@ ranReportGsmColumns = [{'name': i, 'id': i} for i in ranReportGsmTable.columns]
 # Top Worst Reports Variables
 neOosReportfilePath = "D:\\ftproot\\configuration_files\\NBI_FM\\" + str(datetime.now().strftime('%Y%m%d')) + "\\"
 topWorstFilePath = "D:\\ftproot\\BSC\\top_worst_report\\"
-current2GTopWorstDcrFile = ""
-current2GTopWorstCssrFile = ""
-current3GTopWorstFile = ""
-current4GTopWorstFile = ""
-#topWorstCurrentDate = str(datetime.now().strftime('%Y%m%d'))
-topWorstCurrentDate = str((datetime.now() - timedelta(days=1)).strftime('%Y%m%d'))
-for file in os.listdir(topWorstFilePath):
-    if topWorstCurrentDate and "2G" and "CSSR" in file:
-        current2GTopWorstCssrFile = file
-    if topWorstCurrentDate and "2G" and "DCR" in file:
-        current2GTopWorstDcrFile = file
-    if topWorstCurrentDate and "3G" in file:
-        current3GTopWorstFile = file
-    if topWorstCurrentDate and "LTE" in file:
-        current4GTopWorstFile = file
-
-#current4GTopWorstDcrDataframe = pd.read_excel(topWorstFilePath + current4GTopWorstFile, sheet_name = 'TOP 50 Drop LTE')
-#current4GTopWorsteRabSrDataframe = pd.read_excel(topWorstFilePath + current4GTopWorstFile, sheet_name = 'TOP 50 E-RAB Setup')
-#current3GTopWorstDataframe = pd.read_excel(topWorstFilePath + current3GTopWorstFile)
-#current2GTopWorstCssrDataframe = pd.read_excel(topWorstFilePath + current2GTopWorstCssrFile)
-#current2GTopWorstDcrDataframe = pd.read_excel(topWorstFilePath + current2GTopWorstDcrFile)
-#
-#topWorst4GeRabSrDataframe = current4GTopWorsteRabSrDataframe.filter(items = ['eNodeB Name', 'Cell FDD TDD Indication', 'Cell Name', 'E-RAB Setup Success Rate (ALL)[%](%)', 'Date'])
-#topWorst4GeRabSrDataframe = topWorst4GeRabSrDataframe.nsmallest(10, 'E-RAB Setup Success Rate (ALL)[%](%)')
-#topWorst4GeRabSrColumns = [{'name': i, 'id': i} for i in topWorst4GeRabSrDataframe.columns]
-#
-#topWorst4GDcrDataframe = current4GTopWorstDcrDataframe.filter(items = ['eNodeB Name', 'Cell FDD TDD Indication', 'Cell Name', 'Call Drop Rate (All)[%]', 'Date'])
-#topWorst4GDcrDataframe = topWorst4GDcrDataframe.nlargest(10, 'Call Drop Rate (All)[%]')
-#topWorst4GDcrColumns = [{'name': i, 'id': i} for i in topWorst4GDcrDataframe.columns]
-#
-#topWorst3GHsdpaCssrDataframe = current3GTopWorstDataframe.filter(items = ['RNC Name', 'NodeB Name', 'Cell Name', 'HSDPA CSSR(%)', 'Date'])
-#topWorst3GHsdpaCssrDataframe = topWorst3GHsdpaCssrDataframe.nsmallest(10, 'HSDPA CSSR(%)')
-#topWorst3GHsdpaCssrColumns = [{'name': i, 'id': i} for i in topWorst3GHsdpaCssrDataframe.columns]
-#
-#topWorst3GHsupaCssrDataframe = current3GTopWorstDataframe.filter(items = ['RNC Name', 'NodeB Name', 'Cell Name', 'HSUPA CSSR(%)', 'Date'])
-#topWorst3GHsupaCssrDataframe = topWorst3GHsupaCssrDataframe.nsmallest(10, 'HSUPA CSSR(%)')
-#topWorst3GHsupaCssrColumns = [{'name': i, 'id': i} for i in topWorst3GHsupaCssrDataframe.columns]
-#
-#topWorst3GUmtsCssrDataframe = current3GTopWorstDataframe.filter(items = ['RNC Name', 'NodeB Name', 'Cell Name', 'Speech CSSR', 'Date'])
-#topWorst3GUmtsCssrDataframe = topWorst3GUmtsCssrDataframe.nsmallest(10, 'Speech CSSR')
-#topWorst3GUmtsCssrColumns = [{'name': i, 'id': i} for i in topWorst3GUmtsCssrDataframe.columns]
-#
-#topWorst3GHsdpaDcrDataframe = current3GTopWorstDataframe.filter(items = ['RNC Name', 'NodeB Name', 'Cell Name', 'HSDPA DCR(%)', 'Date'])
-#topWorst3GHsdpaDcrDataframe = topWorst3GHsdpaDcrDataframe.nlargest(10, 'HSDPA DCR(%)')
-#topWorst3GHsdpaDcrColumns = [{'name': i, 'id': i} for i in topWorst3GHsdpaDcrDataframe.columns]
-#
-#topWorst3GHsupaDcrDataframe = current3GTopWorstDataframe.filter(items = ['RNC Name', 'NodeB Name', 'Cell Name', 'HSUPA DCR(%)', 'Date'])
-#topWorst3GHsupaDcrDataframe = topWorst3GHsupaDcrDataframe.nlargest(10, 'HSUPA DCR(%)')
-#topWorst3GHsupaDcrColumns = [{'name': i, 'id': i} for i in topWorst3GHsupaDcrDataframe.columns]
-#
-#topWorst3GUmtsDcrDataframe = current3GTopWorstDataframe.filter(items=['RNC Name', 'NodeB Name', 'Cell Name', 'Speech DCR(%)', 'Date'])
-#topWorst3GUmtsDcrDataframe = topWorst3GUmtsDcrDataframe.nlargest(10, 'Speech DCR(%)')
-#topWorst3GUmtsDcrColumns = [{'name': i, 'id': i} for i in topWorst3GUmtsDcrDataframe.columns]
-#
-#topWorst2GSpeechCssrDataframe = current2GTopWorstCssrDataframe.filter(items = ['GBSC', 'Site Name', 'Cell Name', 'Call Setup Success Rate – Speech (%)', 'Date'])
-#topWorst2GSpeechCssrDataframe = topWorst2GSpeechCssrDataframe.nsmallest(10, 'Call Setup Success Rate – Speech (%)')
-#topWorst2GSpeechCssrColumns = [{'name': i, 'id': i} for i in topWorst2GSpeechCssrDataframe.columns]
-#
-#topWorst2GSpeechDcrDataframe = current2GTopWorstDcrDataframe.filter(items = ['GBSC', 'Site Name', 'Cell Name', 'Drop Call Rate – Speech (%)', 'Date'])
-#topWorst2GSpeechDcrDataframe = topWorst2GSpeechDcrDataframe.nlargest(10, 'Drop Call Rate – Speech (%)')
-#topWorst2GSpeechDcrColumns = [{'name': i, 'id': i} for i in topWorst2GSpeechDcrDataframe.columns]
 
 app.layout = html.Div(children=[
     html.Div(
@@ -238,9 +180,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst LTE eRAB SR'),
                     dash_table.DataTable(
-                        id = 'topWorst4GeRabSrTable',
-                        #columns = topWorst4GeRabSrColumns,
-                        #data = topWorst4GeRabSrDataframe.to_dict('records')
+                        id = 'topWorst4GeRabSrTable'
                     )
                 ]
             ),
@@ -249,9 +189,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst LTE DCR'),
                     dash_table.DataTable(
-                        id='topWorst4GDcrTable',
-                        #columns=topWorst4GDcrColumns,
-                        #data=topWorst4GDcrDataframe.to_dict('records')
+                        id='topWorst4GDcrTable'
                     )
                 ]
             ),
@@ -260,9 +198,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst HSDPA CSSR'),
                     dash_table.DataTable(
-                        id = 'topWorst3GHsdpaCssrTable',
-                        #columns = topWorst3GHsdpaCssrColumns,
-                        #data = topWorst3GHsdpaCssrDataframe.to_dict('records')
+                        id = 'topWorst3GHsdpaCssrTable'
                     )
                 ]
             ),
@@ -271,9 +207,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst HSUPA CSSR'),
                     dash_table.DataTable(
-                        id='topWorst3GHsupaCssrTable',
-                        #columns=topWorst3GHsupaCssrColumns,
-                        #data=topWorst3GHsupaCssrDataframe.to_dict('records')
+                        id='topWorst3GHsupaCssrTable'
                     )
                 ]
             ),
@@ -282,9 +216,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst UMTS CSSR'),
                     dash_table.DataTable(
-                        id='topWorst3GUmtsCssrTable',
-                        #columns=topWorst3GUmtsCssrColumns,
-                        #data=topWorst3GUmtsCssrDataframe.to_dict('records')
+                        id='topWorst3GUmtsCssrTable'
                     )
                 ]
             ),
@@ -293,9 +225,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst HSDPA DCR'),
                     dash_table.DataTable(
-                        id='topWorst3GHsdpaDcrTable',
-                        #columns=topWorst3GHsdpaDcrColumns,
-                        #data=topWorst3GHsdpaDcrDataframe.to_dict('records')
+                        id='topWorst3GHsdpaDcrTable'
                     )
                 ]
             ),
@@ -304,9 +234,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst HSUPA DCR'),
                     dash_table.DataTable(
-                        id='topWorst3GHsupaDcrTable',
-                        #columns=topWorst3GHsupaDcrColumns,
-                        #data=topWorst3GHsupaDcrDataframe.to_dict('records')
+                        id='topWorst3GHsupaDcrTable'
                     )
                 ]
             ),
@@ -315,9 +243,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst UMTS DCR'),
                     dash_table.DataTable(
-                    #    id='topWorst3GUmtsDcrTable',
-                    #    columns=topWorst3GUmtsDcrColumns,
-                    #    data=topWorst3GUmtsDcrDataframe.to_dict('records')
+                    #    id='topWorst3GUmtsDcrTable'
                     )
                 ]
             ),
@@ -326,9 +252,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst GSM CSSR'),
                     dash_table.DataTable(
-                        id='topWorst2GSpeechCssrTable',
-                        #columns=topWorst2GSpeechCssrColumns,
-                        #data=topWorst2GSpeechCssrDataframe.to_dict('records')
+                        id='topWorst2GSpeechCssrTable'
                     )
                 ]
             ),
@@ -337,9 +261,7 @@ app.layout = html.Div(children=[
                 children = [
                     html.H3('Top Worst GSM DCR'),
                     dash_table.DataTable(
-                        id='topWorst2GSpeechDcrTable',
-                        #columns=topWorst2GSpeechDcrColumns,
-                        #data=topWorst2GSpeechDcrDataframe.to_dict('records')
+                        id='topWorst2GSpeechDcrTable'
                     )
                 ]
             )
@@ -456,7 +378,8 @@ app.layout = html.Div(children=[
         Input('dataUpateInterval', 'n_intervals'), 
         Input('timeFrameDropdown', 'value'), 
         Input('dataTypeDropdown', 'value')
-    ])
+    ]
+)
 def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
     gsmGraphValueConversionDict = {'CS Call Setup Success Rate':'cssr', 'PS Call Setup Success Rate':'edgedlssr', 'CS Drop Call Rate':'dcr', 'PS Drop Call Rate':'edgedldcr', 'Assignment Success Rate':'assignmentsuccessrate', 'Location Update Success Rate':'luupdatesr'}
     umtsGraphValueConversionDict = {'CS Call Setup Success Rate':'csconnectionsuccessrate', 'PS Call Setup Success Rate':'psrtsuccessrate', 'CS Drop Call Rate':'csdropcallrate', 'PS Drop Call Rate':'psdropcallrate', 'Assignment Success Rate':'rrcconnectionsuccessrate', 'Location Update Success Rate':'pagingsuccessrate'}
@@ -571,6 +494,7 @@ def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
     connectr.close()
     return bscfig, rncfig, trxUsageGraph, oosNeGraph, cssrNetworkWideGraph
 
+# Callback to update top worst data tables
 @app.callback([
         Output('topWorst4GeRabSrTable', 'columns'),
         Output('topWorst4GeRabSrTable', 'data'),
@@ -599,14 +523,11 @@ def refreshTopWorstTableContent(currentTab):
     # Ensure to refresh top worst tables only if that tab is selected
     if currentTab == 'Top Worst Reports':
         # Top Worst Reports Variables
-        #neOosReportfilePath = "D:\\ftproot\\configuration_files\\NBI_FM\\" + str(datetime.now().strftime('%Y%m%d')) + "\\"
-        #topWorstFilePath = "D:\\ftproot\\BSC\\top_worst_report\\"
         current2GTopWorstDcrFile = ""
         current2GTopWorstCssrFile = ""
         current3GTopWorstFile = ""
         current4GTopWorstFile = ""
         topWorstCurrentDate = str(datetime.now().strftime('%Y%m%d'))
-        #topWorstCurrentDate = str((datetime.now() - timedelta(days=1)).strftime('%Y%m%d'))
         for file in os.listdir(topWorstFilePath):
             if topWorstCurrentDate and "2G" and "CSSR" in file:
                 current2GTopWorstCssrFile = file
@@ -666,6 +587,7 @@ def refreshTopWorstTableContent(currentTab):
     else:
         pass
 
+# Callback to hide/display selected tab
 @app.callback([
     Output('graphGridContainer', 'style'),
     Output('datatableGridContainer', 'style'), 
