7474a2b822d289540ddf249f84c3fba41504e918
Carlos Portes <carlosportes.c@gmail.com>
2021-01-27 10:24:38 -0400

    2021012701: Datatable styling
    -WIP: Datatable styling. TOP graph disabled because of performance issues.

--- dashboard.py
+++ dashboard.py
@@ -629,38 +629,40 @@ def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
     dcrNetworkWideGraph = make_subplots(rows = 1, cols = 1, shared_xaxes = True, shared_yaxes = True)
     volteDcrNetworkWideGraph = make_subplots(rows = 1, cols = 1, shared_xaxes = True, shared_yaxes = True)
     startTimeNetworkWide = (datetime.now()-timedelta(days=3)).strftime("%Y-%m-%d")
-    # Query TOP DCR cell names from DB
-    pointer.execute('select b.time,b.cellname, b.datadcr from (select a.time,a.cellname,a.datadcr,row_number() over (partition by a.time order by a.datadcr desc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    topWorst4GDcrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'datadcr'])
-    queryRaw.clear()
-    # Query TOP VoLTE DCR cell names from DB
-    pointer.execute('select b.time,b.cellname, b.voltedcr from (select a.time,a.cellname,a.voltedcr,row_number() over (partition by a.time order by a.voltedcr desc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    topWorst4GVolteDcrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'voltedcr'])
-    queryRaw.clear()
-    # Query TOP eRAB SR cell names from DB
-    pointer.execute('select b.time,b.cellname, b.dataerabssr from (select a.time,a.cellname,a.dataerabssr,row_number() over (partition by a.time order by a.dataerabssr asc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    topWorst4GeRabSrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'dataerabssr'])
-    queryRaw.clear()
-    # Query TOP VoLTE eRAB SR cell names from DB
-    pointer.execute('select b.time,b.cellname, b.volteerabssr from (select a.time,a.cellname,a.volteerabssr,row_number() over (partition by a.time order by a.volteerabssr asc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
-    queryRaw = pointer.fetchall()
-    queryPayload = np.array(queryRaw)
-    topWorst4GvolteeRabSrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'volteerabssr'])
-    queryRaw.clear()
+    ## Query TOP DCR cell names from DB
+    #pointer.execute('select b.time,b.cellname, b.datadcr from (select a.time,a.cellname,a.datadcr,row_number() over (partition by a.time order by a.datadcr desc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
+    #queryRaw = pointer.fetchall()
+    #queryPayload = np.array(queryRaw)
+    #topWorst4GDcrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'datadcr'])
+    #queryRaw.clear()
+    ## Query TOP VoLTE DCR cell names from DB
+    #pointer.execute('select b.time,b.cellname, b.voltedcr from (select a.time,a.cellname,a.voltedcr,row_number() over (partition by a.time order by a.voltedcr desc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
+    #queryRaw = pointer.fetchall()
+    #queryPayload = np.array(queryRaw)
+    #topWorst4GVolteDcrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'voltedcr'])
+    #queryRaw.clear()
+    ## Query TOP eRAB SR cell names from DB
+    #pointer.execute('select b.time,b.cellname, b.dataerabssr from (select a.time,a.cellname,a.dataerabssr,row_number() over (partition by a.time order by a.dataerabssr asc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
+    #queryRaw = pointer.fetchall()
+    #queryPayload = np.array(queryRaw)
+    #topWorst4GeRabSrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'dataerabssr'])
+    #queryRaw.clear()
+    ## Query TOP VoLTE eRAB SR cell names from DB
+    #pointer.execute('select b.time,b.cellname, b.volteerabssr from (select a.time,a.cellname,a.volteerabssr,row_number() over (partition by a.time order by a.volteerabssr asc) as rn from ran_pf_data.ran_report_4g_report_specific a where a.time >= \'' + str(startTimeNetworkWide) + '\') b where b.rn = 1')
+    #queryRaw = pointer.fetchall()
+    #queryPayload = np.array(queryRaw)
+    #topWorst4GvolteeRabSrPerHourDataFrame = pd.DataFrame(queryPayload, columns=['time', 'cellname', 'volteerabssr'])
+    #queryRaw.clear()
     for band in lteBandList:
         pointer.execute('SELECT time,erabssr,dcr FROM ran_pf_data.ran_report_4g_report_network_wide where ltecellgroup = \'' + band + '\' and time > \'' + str(startTimeNetworkWide) + '\';')
         queryRaw = pointer.fetchall()
         queryPayload = np.array(queryRaw)
         # Transform the query payload into a dataframe
         lteDataDataframe = pd.DataFrame(queryPayload, columns=['time', 'erabssr', 'dcr'])
-        cssrNetworkWideGraph.add_trace(go.Scatter(x=lteDataDataframe['time'], y=lteDataDataframe['erabssr'], name=band, text=topWorst4GeRabSrPerHourDataFrame['cellname']))
-        dcrNetworkWideGraph.add_trace(go.Scatter(x=lteDataDataframe['time'], y=lteDataDataframe['dcr'], name=band, text=topWorst4GDcrPerHourDataFrame['cellname']))
+        #cssrNetworkWideGraph.add_trace(go.Scatter(x=lteDataDataframe['time'], y=lteDataDataframe['erabssr'], name=band, text=topWorst4GeRabSrPerHourDataFrame['cellname']))
+        #dcrNetworkWideGraph.add_trace(go.Scatter(x=lteDataDataframe['time'], y=lteDataDataframe['dcr'], name=band, text=topWorst4GDcrPerHourDataFrame['cellname']))
+        cssrNetworkWideGraph.add_trace(go.Scatter(x=lteDataDataframe['time'], y=lteDataDataframe['erabssr'], name=band))
+        dcrNetworkWideGraph.add_trace(go.Scatter(x=lteDataDataframe['time'], y=lteDataDataframe['dcr'], name=band))
         queryRaw.clear()
         # Since there's no VoLTE on B42, we must skip it from the VolTE graphs
         if band != 'Network Band=42':
@@ -669,8 +671,10 @@ def updateGraphData_bsc(currentInterval, timeFrameDropdown, dataTypeDropdown):
             queryPayload = np.array(queryRaw)
             # Transform the query payload into a dataframe
             wttxDataDataframe = pd.DataFrame(queryPayload, columns=['time', 'volteerabssr', 'voltedcr'])
-            volteCssrNetworkWideGraph.add_trace(go.Scatter(x=wttxDataDataframe['time'], y=wttxDataDataframe['volteerabssr'], name=band, text=topWorst4GVolteDcrPerHourDataFrame['cellname']))
-            volteDcrNetworkWideGraph.add_trace(go.Scatter(x=wttxDataDataframe['time'], y=wttxDataDataframe['voltedcr'], name=band, text=topWorst4GvolteeRabSrPerHourDataFrame['cellname']))
+            #volteCssrNetworkWideGraph.add_trace(go.Scatter(x=wttxDataDataframe['time'], y=wttxDataDataframe['volteerabssr'], name=band, text=topWorst4GVolteDcrPerHourDataFrame['cellname']))
+            #volteDcrNetworkWideGraph.add_trace(go.Scatter(x=wttxDataDataframe['time'], y=wttxDataDataframe['voltedcr'], name=band, text=topWorst4GvolteeRabSrPerHourDataFrame['cellname']))
+            volteCssrNetworkWideGraph.add_trace(go.Scatter(x=wttxDataDataframe['time'], y=wttxDataDataframe['volteerabssr'], name=band))
+            volteDcrNetworkWideGraph.add_trace(go.Scatter(x=wttxDataDataframe['time'], y=wttxDataDataframe['voltedcr'], name=band))
             queryRaw.clear()
         queryRaw.clear()
     cssrNetworkWideGraph.update_layout(
