c8fe40d29a8050a7943e5e3b4ce27587e3e93777
Carlos Portes <carlosportes.c@gmail.com>
2021-04-09 12:09:48 -0400

    2021040902: ftp file download
    -Complete: FTP login, file download and dataframe conversio

--- test.py
+++ test.py
@@ -1,15 +1,30 @@
 from ftplib import FTP
-class ftpCredentials():
+from io import BytesIO
+import pandas as pd
+
+class ranFtpCredentials():
     hostname = 'bscserver'
     username = 'sitedb'
     password = 'BSCAltice.123'
-    topWorstFilePath = '/BSC/top_worst_report/'
 
-ftpLogin = ftpCredentials()
-# Instantiate FTP connection
-ftp = FTP(host=ftpLogin.hostname)
-ftp.login(user=ftpLogin.username, passwd=ftpLogin.password)
-# Move to desired path
-ftp.cwd(ftpLogin.topWorstFilePath)
-print(ftp.pwd())
-ftp.quit()
+topWorstFilePath = '/BSC/'
+fileName = 'testExcel.xlsx'
+
+def downloadFtpFile(ftpLogin, filePath, fileName):
+    # Instantiate FTP connection
+    ftp = FTP(host=ftpLogin.hostname)
+    ftp.login(user=ftpLogin.username, passwd=ftpLogin.password)
+    # Move to desired path
+    ftp.cwd(filePath)
+    # Instantiate a BytesIO object to temp store the xlsx file from the FTP server
+    b = BytesIO()
+    # Return file as binary with retrbinary functon. Must send according RETR command as part of FTP protocol
+    ftp.retrbinary('RETR ' + fileName, b.write)
+    # Open as Dataframe
+    dataframe = pd.read_excel(b)
+    ftp.quit()
+    return dataframe
+    
+ftpLogin = ranFtpCredentials()
+data = downloadFtpFile(ftpLogin, topWorstFilePath, fileName)
+print(data)
